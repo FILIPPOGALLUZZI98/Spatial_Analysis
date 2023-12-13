dffb666913373a53f389ce8e0931ed45b588e744
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-13 21:55:17 +0100

    Create 102.Vector.R

---
+++ 102.Vector.R
@@ -0,0 +1,19 @@
+# The 'terra' package defines a set of classes to represent spatial data. A class defines a 
+# particular data type. The data.frame is an example of a class. Any particular data.frame 
+# you create is an object (instantiation) of that class.
+# 'terra' introduces a number of classes with names that start with 'Spat'. For vector data, 
+# the relevant class is 'SpatVector'. These classes represent geometries as well as attributes
+# (variables) describing the geometries
+
+
+
+
+
+
+
+
+
+
+
+
+
