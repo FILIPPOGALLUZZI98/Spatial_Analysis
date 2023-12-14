3960cc4bf27e14e3d91f6eab4e384fc207b63c2b
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-14 14:05:22 +0100

    Update 101.Vector_&_Raster.R

--- 101.Vector_&_Raster.R
+++ 101.Vector_&_Raster.R
@@ -2,30 +2,33 @@
 library(terra)
 # A map is a plot of geospatial data that also has labels and other graphical objects such 
 # as a scale bar or legend. The spatial data itself should not be referred to as a map.
+# 'terra' introduces a number of classes with names that start with 'Spat'
 
 #################################################################################################
 #################################################################################################
 ##  VECTOR
-# The main vector data types are points, lines and polygons. In all cases,
-# the geometry of these data structures consists of sets of coordinate pairs (x, y).
-# A polygon refers to a set of closed polylines
-
-# 'terra' introduces a number of classes with names that start with 'Spat'. For vector data, 
-# the relevant class is 'SpatVector'. These classes represent geometries as well as attributes
-# (variables) describing the geometries
+# The main vector data types are points, lines and polygons. The geometry of these data structures 
+# consists of sets of coordinate pairs (x, y)
 
 # To create a 'SpatVector' (punti)
-points<-vect(data)  ## Dove 'data' è un vettore, una lista, una matrice o un file
+points<-vect(data)  ## Dove 'data' è un vettore, una lista, una matrice
+# Oppure a partire da un file presente nella cartella di lavoro
+v<-vect(nome_file) 
+
 # Per fornire un sistema di riferimento
 crdref<-"+proj=longlat +datum=WGS84"  ## Selezioniamo il datum, in quest caso 'WGS84'
-points<- vect(data, crs=crdref)  ## Specifichiamo il sistema di riferimento nel vettore
-crs(points)  ## Get or set the coordinate reference system of a SpatRaster or SpatVector.
+v<- vect(nome_file, crs=crdref)  ## Specifichiamo il sistema di riferimento nel vettore
+crs(v)  ## Get or set the coordinate reference system of a SpatRaster or SpatVector.
 # Per unire 'SpatVector' e dataframe (con stesso numero di righe)
-new_points<- vect(data, atts=dataframe, crs=crdref)
+new_v<- vect(nome_file, atts=dataframe, crs=crdref)
 
 # Per fare delle linee o poligoni
-lines<-vect(data, type="lines", crs=crdref)
-polig<-vect(data, type="polygons", crs=crdref)
+lines<-vect(nome_file, type="lines", crs=crdref)
+polig<-vect(nome_file, type="polygons", crs=crdref)
+
+# You can write new files using the 'writeVector method'
+# You need to add argument 'overwrite=TRUE' if you want to overwrite an existing file
+writeVector(v, "nome_nuovo_file.shp")  ## 'v' è un 'SpatVector'
 
 #################################################################################################
 #################################################################################################
@@ -40,14 +43,18 @@ polig<-vect(data, type="polygons", crs=crdref)
 # the size of the raster cells can be computed. While raster cells can be thought of as a
 # set of regular polygons, it would be very inefficient to represent the data that way as 
 # coordinates for each cell would have to be stored explicitly. Doing so would also
-# dramatically increase processing time.
+# dramatically increase processing time
 
-# A 'SpatRaster' represents multi-variable raster data
 # To create a raster data
 r<-rast(ncol=10, nrow=10, xmin=-150, xmax=-80, ymin=20, ymax=60)
 # In questo caso è vuoto; per assegnare valori
 values(r)<-A  ## Posso prenderli da un dataset, da un file, etc.
+# Oppure a partire da un file presente nella cartella di lavoro
+r<-rast(nome_file) 
 
+# You can write new files using the 'writeRaster' method
+# You need to add argument 'overwrite=TRUE' if you want to overwrite an existing file
+writeRaster(r, "nome_nuovo_file.tif)  ## 'r' è un 'SpatRaster'
 
 #######################################################################################################
 #######################################################################################################
