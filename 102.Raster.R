f9fce57d6f9cd17f0f9c73544c6a368b5e6ab9c0
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-14 15:17:23 +0100

    Update 102.Raster.R

--- 102.Raster.R
+++ 102.Raster.R
@@ -1,28 +1,56 @@
-##  RASTER
-# A raster divides the world into a grid of equally sized rectangles (cells or pixels) that all have one or
-# more values for the variables of interest. A raster cell value should normally represent the average value
-# for the area it covers. However, in some cases the values are actually estimates for the center of the cell
-# In contrast to vector data, in raster data the geometry is not explicitly stored as coordinates. It is
-# implicitly set by knowing the spatial extent and the number or rows and columns in which the area is divided
-# From the extent and number of rows and columns, the size of the raster cells can be computed. While raster cells
-# can be thought of as a set of regular polygons, it would be very inefficient to represent the data that way as 
-# coordinates for each cell would have to be stored explicitly. Doing so would also dramatically increase processing time
+# Un oggetto raster divide il mondo in una griglia di rettangoli (pixel) di uguale grandezza, ognuno dei quali
+# ha uno o più valori delle variabili di interesse. Il valore può essere il valore medio o il valore al centro.
+# A differenza dei vector data, la geometria non è esplicitamente salvata come coordinate, ma è implicitamente
+# settata dalla conscenza dell'estensione spaziale ed il numero di righe/colonne in cui l'area è divisa
 
-# To create a raster data
+# Per conoscere le informazioni base
+cell(r); res(r); col(r), row(r); ext(r)
 
+# Create a raster data
+# Le impostazioni di default creano una struttura raster globale senza un SRD lon/lat e gradi delle celle 1x1 
+r<-rast()  ## Crea un raster con le impostazioni di default
+r<-rast(ncol=n, nrow=m, xmin=XXX, xmax=XXX, ymin=YYY, ymax=YYY)
+# Per cambiare i parametri (per esempio la risoluzione)
+res(r)<-XXX
+# Questo oggetto consiste soltanto nella geometria raster (righe e colonne), ma è vuoto
+# Per inserire i valori usiamo 'values()'
+values(r)<-valori
+# Values are not lost when changing the extent as this change adjusts the resolution, but does not change the
+# number of rows or columns
+ext(r)<-XXX
 
-
-
-
-
-r<-rast(ncol=10, nrow=10, xmin=-150, xmax=-80, ymin=20, ymax=60)
-# In questo caso è vuoto; per assegnare valori
-values(r)<-A  ## Posso prenderli da un dataset, da un file, etc.
-# Oppure a partire da un file presente nella cartella di lavoro
+# A partire da un file presente nella cartella di lavoro
 r<-rast(nome_file) 
 
 # You can write new files using the 'writeRaster' method
 # You need to add argument 'overwrite=TRUE' if you want to overwrite an existing file
 writeRaster(r, "nome_nuovo_file.tif)  ## 'r' è un 'SpatRaster'
+
+######################################################################################################################
+######################################################################################################################
+## OPERAZIONI SUL RASTER 
+# The 'crop' function lets you take a geographic subset of a larger raster object. 
+# The 'merge' function lets you merge 2 or more SpatRaster objects into a single new object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
