37ab41ba24630558c9bf49a6c2130ad45e10b0a4
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-14 13:57:47 +0100

    Update 101.Vector_&_Raster.R

--- 101.Vector_&_Raster.R
+++ 101.Vector_&_Raster.R
@@ -43,9 +43,36 @@ polig<-vect(data, type="polygons", crs=crdref)
 # dramatically increase processing time.
 
 # A 'SpatRaster' represents multi-variable raster data
+# To create a raster data
 r<-rast(ncol=10, nrow=10, xmin=-150, xmax=-80, ymin=20, ymax=60)
 # In questo caso è vuoto; per assegnare valori
 values(r)<-A  ## Posso prenderli da un dataset, da un file, etc.
+
+
+#######################################################################################################
+#######################################################################################################
+##  BASIC EXAMPLE
+#  Location (longitude and latitude) of 10 weather stations (A to J) and their annual precipitation.
+name <- LETTERS[1:10]  ## Per fare un vettore di lettere da A a J
+longitude <- c(-116.7, -120.4, -116.7, -113.5, -115.5,
+               -120.8, -119.5, -113.7, -113.7, -110.7)
+latitude <- c(45.3, 42.6, 38.9, 42.1, 35.7, 38.9,
+              36.2, 39, 41.6, 36.9)
+stations <- cbind(longitude, latitude)  ## Unisco i due vettori e formo un dataframe
+# To simulate rainfall data
+set.seed(0)  ## Per fare in modo che ad ogni run non vengano cambiati i numeri generati
+# Vettore di lunghezza 10 (latitude points) contenente numeri random (0-1000) arrotondati
+precip <- round((runif(length(latitude))*10)^3)  
+# Plot that shows the location of the weather stations, and the size of the dots is proportional
+# to the amount of precipitation
+psize <- 1 + precip/500  ## In questo modo ottengo un vettore con la grandezza dei punti
+plot(stations, cex=psize, pch=20, col='red', main='Precipitation')  ## Imposto cex=psize
+# Add names to plot
+text(stations, name, pos=4)
+# add a legend
+leg <- c(100, 250, 500, 1000)
+legend.psize <- 1+leg/500
+legend("topright", legend=leg, pch=20, pt.cex=legend.psize, col='red', bg='gray')
 
 
 
