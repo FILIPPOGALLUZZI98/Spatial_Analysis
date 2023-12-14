f7575364f75ba65b35b52cb34075c4cb55ae9833
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-14 13:57:57 +0100

    Update 103.Reading_&_Writing.R

--- 103.Reading_&_Writing.R
+++ 103.Reading_&_Writing.R
@@ -1,16 +1,3 @@
-# Nel caso di vector data
-a<-vect(nome_file)
-# You can write new files using the 'writeVector method'
-# You need to add argument 'overwrite=TRUE' if you want to overwrite an existing file
-XXX<-"nome_nuovo_file.shp"
-writeVector(a, XXX)  ## 'a' è un 'SpatVector'
-
-
-# Nel caso di raster data
-a<-rast(nome_file)
-# You can write new files using the 'writeRaster' method
-# You need to add argument 'overwrite=TRUE' if you want to overwrite an existing file
-a<-writeRaster(a, "nome_file.tif)  ## 'a' è un 'SpatRaster'
 
 
 
