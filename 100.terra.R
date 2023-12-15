4d008f36f09bfbbae64b6bc5959aa57576ff137b
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-15 13:40:04 +0100

    Update 100.terra.R

--- 100.terra.R
+++ 100.terra.R
@@ -1,2 +1,9 @@
-# Il pacchetto 'terra' è implicito in tutti i codici
+# Il pacchetto 'terra' è implicito in tutti i codici--> https://cran.r-project.org/web/packages/terra/index.html
+library(terra)
+# Methods for spatial data analysis with vector (points, lines, polygons) and raster (grid) data
+
+
+
+
+
 
