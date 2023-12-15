97672f66c411eb4b2fad660445d9dc7b7a8cb0d5
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-15 15:18:25 +0100

    Update 202.Autocorrelation.R

--- 202.Autocorrelation.R
+++ 202.Autocorrelation.R
@@ -1,1 +1,19 @@
+# Autocorrelation is a measure of similarity (correlation) between nearby observations
+
+## TEMPORAL AUTOCORRELATION
+# Sia 'd' un vettore di osservazioni temporali (daily)
+set.seed(0)
+d <- sample(100, 10)
+a <- d[-length(d)]
+b <- d[-1]
+plot(a, b, xlab='t', ylab='t-1')
+
+
+
+
+
+
+
+
+
 
