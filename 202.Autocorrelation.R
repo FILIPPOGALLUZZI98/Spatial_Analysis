184e0063e7ef704397c27fd378dedccff5c1ae9c
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2023-12-16 16:04:54 +0100

    Update 202.Autocorrelation.R

--- 202.Autocorrelation.R
+++ 202.Autocorrelation.R
@@ -8,10 +8,17 @@ b<-sample(100,10)
 plot(a, b, xlab='b', ylab='a', pch=20, col="blue")
 cor(a,b)  ## La correlazione è bassa, dato che sono vettori casuali
 
-# Se ora ordiniamo gli elementi, la correlazione aumenta
-a<-sort(a)
-b<-sort(b)
-plot(a, b, xlab='b', ylab='a', pch=20, col="blue")
+# Se vogliamo fare la correlazione spaziale, è più difficile perché dobbiamo considerare
+# più dimensioni
+# Measures of spatial autocorrelation describe the degree two which observations (values)
+# at spatial locations, are similar to each other. So we need two things: observations and locations.
+# A commonly used statistic that describes spatial autocorrelation is Moran’s I
+# Consideriamo il seguente esempio
+p<-vect(system.file("ex/lux.shp", package="terra"))  ## Importiamo i dati da 'terra'
+p<-p[p$NAME_1=="Diekirch", ]  ## Selezioniamo la variabile NAME_1 con i valori "Diekirch"
+p$value<-c(10, 6, 4, 11, 6)  ## Creiamo una nuova variabile
+as.data.frame(p)  ## Mostriamo 'p' come dataframe
+# Supponiamo di essere interessati all'autocorrelazione spaziale della variabile "AREA"
 
 
 
