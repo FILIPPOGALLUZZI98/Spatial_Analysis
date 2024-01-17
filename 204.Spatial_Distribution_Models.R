207dfe5e7c4adcd7ecb5d64531366e6ddcd539f4
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-01-17 12:29:03 +0100

    Update 204.Spatial_Distribution_Models.R

--- 204.Spatial_Distribution_Models.R
+++ 204.Spatial_Distribution_Models.R
@@ -10,14 +10,47 @@ library(geodata)
 
 # PER PLOTTARE I CONFINI DEGLI STATI!!
 wrld <- geodata::world(path=".")
+plot(wrld)  ## Vediamo i confini degli stati
+# Se vogliamo vedere solo un certo stato:
 bnds <- wrld[wrld$NAME_0 %in% c("Stato da plottare", "Stato da Plottare 2"), ...]
+plot(bnds)
+# Altrimenti, se vogliamo sovrapporlo alle osservazioni:
 lines(bnds)
 
-# PER PLOTTARE LE OSSERVAZIONI DI UNA SPECIE
-plot(bf[,1:2], cex=0.5, col="red")
-bf <- spat_data("bigfoot")
-dim(bf)
-bf
 
-# Let's plot the locations
+
+# PER PLOTTARE LE OSSERVAZIONI DI BIGFOOT
+bf <- spat_data("bigfoot")
+plot(bf[,1:2], cex=0.5, col="red")
+wrld <- geodata::world(path=".")
+bnds <- wrld[wrld$NAME_0 %in% c("Canada", "Mexico", "United States"), ]
+lines(bnds)
+
+
+# PREDITTORI
+# As is common in species distribution modeling, we use climate data as predictor variables
+# https://www.worldclim.org/data/bioclim.html
+wc <- geodata::worldclim_global("bio", res=10, ".")  ## "bio"->bioclimatic variables; 10->risoluzione in gradi
+plot(wc[[c(1, 12)]], nr=2)  ## [c(1,12)]--> seleziono soltanto i predittori 1 e 12
+# Adesso vogliamo estrarre i dati climatici per la zona che ci interessa
+bfc <- extract(wc, bf[,1:2])  ## ????
+bfc <- bfc[,-1]  ## To remove the first column with the ID that we don't need
+plot(bfc[ ,"wc2.1_10m_bio_1"], bfc[, "wc2.1_10m_bio_12"], col="red",
+        xlab="Annual mean temperature (°C)", ylab="Annual precipitation (mm)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
