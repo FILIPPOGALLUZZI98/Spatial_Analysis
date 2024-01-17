8880d8e2339b8b529bec7d4ca022e89f1b21d7fb
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-01-17 12:41:41 +0100

    Update 301.Remote_Sensing.R

--- 301.Remote_Sensing.R
+++ 301.Remote_Sensing.R
@@ -1,1 +1,30 @@
+# Important properties to consider while searching the remotely sensed (satellite) data include:
+# Spatial resolution --> the size of the grid cells
+# Temporal resolution --> the return time or frequency that data is collected
+# Spectral resolution --> the parts of the electromagnetic spectrum for which measurements are made
+# Radiometric resolution (sensor sensitivity) --> ability to measure small differences
+# Quality issues --> the presence of cloud-cover or of artifacts in the data
+
+# The Landsat program started in 1972 and is is the longest running Earth-observation satellite program
+
+# Public satellite data:
+# http://earthexplorer.usgs.gov/
+# https://lpdaacsvc.cr.usgs.gov/appeears/
+# https://search.earthdata.nasa.gov/search
+# https://lpdaac.usgs.gov/data_access/data_pool
+# https://scihub.copernicus.eu/
+# https://aws.amazon.com/public-data-sets/landsat/
+
+# Most remote sensing products consist of observations of reflectance data. That is, they are measures 
+# of the intensity of the sun’s radiation that is reflected by the earth. Reflectance is normally measured 
+# for different wavelengths of the electromagnetic spectrum. For example, it can be measured in the near-infrared,
+# red, green, and blue wavelengths. If that is the case, satellite data can be referred to as “multi-spectral”
+# The data are normally stored as raster data, and are generally referred to as “images”.
+
+
+
+
+
+
+
 
