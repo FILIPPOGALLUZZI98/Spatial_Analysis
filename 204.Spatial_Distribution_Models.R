f3b7909608afcfaa14cec4566b4daf3185199f8c
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-01-17 12:09:39 +0100

    Update 204.Spatial_Distribution_Models.R

--- 204.Spatial_Distribution_Models.R
+++ 204.Spatial_Distribution_Models.R
@@ -1,1 +1,21 @@
+# Bigfoot observations!
+if (!require("rspat")) remotes::install_github("rspatial/rspat")
+if (!require("predicts")) install.packages("predicts")
+if (!require("geodata")) install.packages("geodata")
+library(terra)
+library(rspat)
+library(geodata)
+
+# PER PLOTTARE I CONFINI DEGLI STATI!!
+wrld <- geodata::world(path=".")
+bnds <- wrld[wrld$NAME_0 %in% c("Stato da plottare", "Stato da Plottare 2"), ...]
+lines(bnds)
+
+# PER PLOTTARE LE OSSERVAZIONI DI UNA SPECIE
+plot(bf[,1:2], cex=0.5, col="red")
+bf <- spat_data("bigfoot")
+dim(bf)
+bf
+
+# Let's plot the locations
 
