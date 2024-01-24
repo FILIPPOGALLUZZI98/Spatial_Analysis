857e6dbc67dcb647cb4abdf79e3964b1f90c17d4
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-01-24 10:47:50 +0100

    Update 202.Image_Properties.R

--- 202.Image_Properties.R
+++ 202.Image_Properties.R
@@ -3,17 +3,15 @@ library(terra)
 library(imageRy)
 
 # Posso quindi creare uno SpatRaster a partire da questi
-# Se non sto usando imageRy allora devo usare la funzione 'rast'
-# b <- rast('data/rs/LC08_044034_20170614_B4.tif')
+# Se non sto usando imageRy allora devo usare la funzione 'rast', per esempio:
+b <- rast('data/rs/LC08_044034_20170614_B4.tif')
 
-# In questo caso (solo in questo caso):
+# In questo caso (solo per imageRy):
 im.list()  ## Per vedere la lista dei dati contenuti in imageRy
 b2<-im.import("sentinel.dolomites.b2.tif")
 b3<-im.import("sentinel.dolomites.b3.tif")
 b4<-im.import("sentinel.dolomites.b4.tif")
 b5<-im.import("sentinel.dolomites.b8.tif")
-# Se vogliamo creare una multi-band image
-s<-c(b2,b3,b4)
 
 # Se vogliamo mostrare i grafici individuali dell'immagine multi-spettrale
 par(mfrow = c(2,2))
@@ -26,19 +24,31 @@ plot(b5, main = "NIR", col = gray(0:100 / 100))
 # because different surface features reflect the incident solar radiation differently. Each layer
 # represent how much incident solar radiation is reflected for a particular wavelength range.
 
+# Se vogliamo creare una multi-band image
+stack<-c(b2, b3, b4, b8)  ## Creo un array con le varie componenti 
+plot(stack)
+plot(stacksent[[4]])  ## Per plottare una componente (NIR in questo caso)
+
+
 # We do not gain that much information from these grey-scale plots; they are often combined into
-# a “composite” to create more interesting plots
+# a “composite” to create more interesting plots.
 # With plotRGB we can combine them into a single composite image. Note that use of strecth = "lin" 
 # (otherwise the image will be pitch-dark)
-plotRGB(s,stretch="lin")
+# Per plottare i primi tre layers in componenti RGB
+plotRGB(stack, r = 1, g = 2, b = 3)
+plotRGB(stack,stretch="lin")  ## Senza specificare le varie bande
 
-# A scatterplot matrix can be helpful in exploring relationships between raster layers. This can 
-# be done with the pairs function.
-pairs(s)
-# Vediamo che il NIR offre informazioni aggiuntive
-s<-c(b2,b3,b4,b5)
-pairs(s)
+# Come visto nel file precendente, per avere info aggiuntive posso spostare le varie bande, per esempio:
+plotRGB(stacksent,r=3,g=2,b=4)  ## Metto il NIR nella componente blu: ciò che riflette NIR diventa blu
+plotRGB(stacksent,r=4,g=2,b=1)  ## Metto il NIR nella componente rossa: ciò che riflette NIR diventa rosso
 
+# Per capire quale delle componenti porta più informazioni:
+pairs(stack)  ## Serve per misurare la correlazione tra più variabili
+# Nella traccia ci sono le varie bande
+# Nei grafici fuori dalla traccia ci sono i coefficienti di pearson e le correlazioni tra le diverse bande
+# Le prime tre bande(quelle visibili) sono molto correlate
+# Una bassa correlazione significa che il NIR porta ulteriori informazioni,
+# mentre le altre portano la stessa quantità di informazioni
 
 
 
