67728c05993cc168ca05296d62d9b994e9d064da
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-01-26 16:51:14 +0100

    Update 306.DCA.R

--- 306.DCA.R
+++ 306.DCA.R
@@ -1,19 +1,21 @@
 # DETRENDED CORRESPONDENCE ANALYSIS AND BASIC RECIPROCAL AVERAGING
-library(vegan)
+# Useremo il dataset 'dune'
 
+library(vegan)
 # Interno a 'vegan' c'è il dataset 'dune' 
 ?dune
 ##  Vegetation and Environment in Dutch Dune Meadows. The dune meadow vegetation data, dune,has
 ##  cover class values of 30 species on 20 sites. The species names are abbreviated to 4+4 letters
-
 # Per usarlo
 data(dune); dune
 
-ord<-decorana(dune)  ## decorana-->detrended correspondence analysis
+# Facciamo la detrended correspondence analysis and basic reciprocal averaging (or orthogonal correspondence analysis)
+ord<-decorana(dune)
+
 # Ciò che stiamo cercando è la lunghezza degli assi
 ord
-
-# Possiamo vedere axis lenghts delle 4 coordinate. Assegnamo la lunghezza a 4 variabili
+# Possiamo leggere axis lenghts delle 4 coordinate
+# Assegnamo la lunghezza a 4 variabili
 ldc1<-3.7004; ldc2<-3.1166; ldc3<-1.30055; ldc4<-1.47888
 # Per calcolare la percentuale
 total=ldc1+ldc2+ldc3+ldc4
