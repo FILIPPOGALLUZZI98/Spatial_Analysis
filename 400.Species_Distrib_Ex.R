923f183910fd632a6d868b560d1abd1e9ff9aa90
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-02-03 15:59:57 +0100

    Update 400.Species_Distrib_Ex.R

--- 400.Species_Distrib_Ex.R
+++ 400.Species_Distrib_Ex.R
@@ -1,1 +1,17 @@
+# This document is based on an analysis of the distribution of wild potato species 
+# The data we will use is available in the rspatial package
+if (!require("rspat")) remotes::install_github('rspatial/rspat')
+library(rspat)
+library(terra)
+
+
+
+
+
+
+
+
+
+
+
 
