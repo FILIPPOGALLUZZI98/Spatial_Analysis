7047740946649c740ec544fa5222f26a38466537
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-02-03 16:01:34 +0100

    Update 400.Species_Distrib_Ex.R

--- 400.Species_Distrib_Ex.R
+++ 400.Species_Distrib_Ex.R
@@ -1,9 +1,14 @@
 # This document is based on an analysis of the distribution of wild potato species 
 # The data we will use is available in the rspatial package
 if (!require("rspat")) remotes::install_github('rspatial/rspat')
-library(rspat)
-library(terra)
+suppressPackageStartupMessages({
+  library(rspat)
+  library(terra)
+})
 
+# Estraiamo i dati che hanno come estenzione '.csv'
+f <- system.file("wildpot.csv", package="rspat")
+basename(f)  ;  v <- read.csv(f)
 
 
 
