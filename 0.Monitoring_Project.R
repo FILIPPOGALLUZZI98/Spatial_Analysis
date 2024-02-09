da34d21f344ac2c65ebc5d075a93dbd3c08d200d
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-02-09 16:11:58 +0100

    Update 0.Monitoring_Project.R

--- 0.Monitoring_Project.R
+++ 0.Monitoring_Project.R
@@ -108,19 +108,43 @@ plot(moist23, col = rev(topo.colors(5)), main = 'NDMI based thresholding 2023')
 
 
 ## VARIABILITY
-cl<-colorRampPalette(viridis(7))(100)
 sd3_S17<-focal(S17_8, matrix(1/9,3,3), fun=sd)
 sd3_S20<-focal(S20_8, matrix(1/9,3,3), fun=sd)
 sd3_S23<-focal(S23_8, matrix(1/9,3,3), fun=sd)
 par(mfrow=c(2,2))
-plot(sd3_S17, col=cl)
+cl<-colorRampPalette(viridis(7))(100)plot(sd3_S17, col=cl)
 plot(sd3_S20, col=cl)
 plot(sd3_S23, col=cl)
 
 
 
 
+## PCA
+S17pc<-im.pca2(S17) 
+S17_pc1<-S17pc$PC1
+S20pc<-im.pca2(S20) 
+S20_pc1<-S20pc$PC1
+S23pc<-im.pca2(S23) 
+S23_pc1<-S23pc$PC1
 
+par(mfrow=c(2,2))
+plot(S17_pc1,col=cl, range=c(-0.3,0.6))
+plot(S20_pc1,col=cl, range=c(-0.3,0.6))
+plot(S23_pc1,col=cl, range=c(-0.3,0.6))
+
+dev.off()
+S17_pc1sd3<-focal(S17_pc1, matrix(1/9, 3, 3), fun=sd)
+S20_pc1sd3<-focal(S20_pc1, matrix(1/9, 3, 3), fun=sd)
+S23_pc1sd3<-focal(S23_pc1, matrix(1/9, 3, 3), fun=sd)
+par(mfrow=c(2,2))
+plot(S23_pc1sd3, col=cl, range=c(0,0.02))
+plot(S17_pc1sd3, col=cl, range=c(0,0.02))
+plot(S20_pc1sd3, col=cl, range=c(0,0.02))
+
+
+
+
+## CLASSIFICATION
 
 
 
