b820521098e1ac3af7a81c814ee8c425cc25f9a0
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-02-16 17:13:51 +0100

    Update 00.Monitoring_Project.R

--- 00.Monitoring_Project.R
+++ 00.Monitoring_Project.R
@@ -3,7 +3,10 @@ library(imageRy)
 library(viridis)
 setwd("D:R_Studio/Immagini_Progetto_Monitoring")
 
-# Le immagini sono prese da sentinel-2a e sono le immagini di aprile per ogni anno dal 2018 al 2023
+
+# ??? Le immagini sono prese da sentinel-2a e sono le immagini di aprile per ogni anno dal 2018 al 2023
+
+
 # Faccio dei cicli for per assegnare le variabili alle immagini scaricate
 nomi_var <- c()
 for (i in 18:23) {
@@ -15,6 +18,23 @@ for (i in 18:23) {
     nomi_file <- c(nomi_file, paste("S", i, "_", j,".tiff", sep=""))}}
 for (i in 1:length(nomi_var)) {
   assign(nomi_var[i], rast(nomi_file[i]))}
+
+
+# PLOT REAL COLOR IMAGES
+S18<-c(S18_4,S18_3,S18_2,S18_8)
+S19<-c(S19_4,S19_3,S19_2,S19_8)
+S20<-c(S20_4,S20_3,S20_2,S20_8)
+S21<-c(S21_4,S21_3,S21_2,S21_8)
+S22<-c(S22_4,S22_3,S22_2,S22_8)
+S23<-c(S23_4,S23_3,S23_2,S23_8)
+par(mfrow=c(2,3))
+plotRGB(S18, stretch="lin", main="April 2018")
+plotRGB(S19, stretch="lin", main="April 2019")
+plotRGB(S20, stretch="lin", main="April 2020")
+plotRGB(S21, stretch="lin", main="April 2021")
+plotRGB(S22, stretch="lin", main="April 2022")
+plotRGB(S23, stretch="lin", main="April 2023")
+dev.off()
 
 
 
