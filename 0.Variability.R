e82f85b72322795f006fa5585070b6e305f6999d
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-02-18 16:47:39 +0100

    Update 0.Variability.R

--- 0.Variability.R
+++ 0.Variability.R
@@ -1,12 +1,18 @@
 ## VARIABILITY
-S18_sd3<-focal(S18, matrix(1/9,3,3), fun=sd)
-S20_sd3<-focal(S20_8, matrix(1/9,3,3), fun=sd)
-S23_sd3<-focal(S23_8, matrix(1/9,3,3), fun=sd)
-par(mfrow=c(2,2))
+S18_sd6<-focal(S18, matrix(1/36,6,6), fun=sd)
+S19_sd6<-focal(S19, matrix(1/36,6,6), fun=sd)
+S20_sd6<-focal(S20, matrix(1/36,6,6), fun=sd)
+S21_sd6<-focal(S21, matrix(1/36,6,6), fun=sd)
+S22_sd6<-focal(S22, matrix(1/36,6,6), fun=sd)
+S23_sd6<-focal(S23, matrix(1/36,6,6), fun=sd)
+par(mfrow=c(2,3))
 cl<-colorRampPalette(viridis(7))(100)
-plot(S18_sd3, col=cl)
-plot(S20_sd3, col=cl)
-plot(S23_sd3, col=cl)
+plot(S18_sd6, col=cl)
+plot(S19_sd6, col=cl)
+plot(S20_sd6, col=cl)
+plot(S21_sd6, col=cl)
+plot(S22_sd6, col=cl)
+plot(S23_sd6, col=cl)
 
 
 
