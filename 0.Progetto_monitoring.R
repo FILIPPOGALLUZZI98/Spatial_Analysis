f769b33ac6d35805a79744a5ebce2c82298394cd
Filippo Galluzzi <146108941+FILIPPOGALLUZZI98@users.noreply.github.com>
2024-02-18 19:06:44 +0100

    Update 0.Progetto_monitoring.R

--- 0.Progetto_monitoring.R
+++ 0.Progetto_monitoring.R
@@ -300,10 +300,12 @@ plot(veg23, col="darkgreen", legend=FALSE, axes=FALSE)
 
 
 
-
+# MONTHLY ANALYSIS 2021-2022
+# da a-f --> 2022
+# da g a n --> 2021
 # Faccio dei cicli for per assegnare le variabili alle immagini scaricate
 nomi_var <- c()
-lett <- c("a", "b", "c", "d", "e", "f")
+lett <- c("a", "b", "c", "d", "e", "f","g","h","i","l","m","n")
 for (i in lett) {
   for (j in c(2, 3, 4, 8)) {
     nomi_var <- c(nomi_var, paste("S", i, "_", j, sep=""))}}
@@ -314,76 +316,52 @@ for (i in lett) {
 for (i in 1:length(nomi_var)) {
   assign(nomi_var[i], rast(nomi_file[i]))}
 
+Sa<-c(Sa_4,Sa_3,Sa_2,Sa_8); Sb<-c(Sb_4,Sb_3,Sb_2,Sb_8); Sc<-c(Sc_4,Sc_3,Sc_2,Sc_8)
+Sd<-c(Sd_4,Sd_3,Sd_2,Sd_8); Se<-c(Se_4,Se_3,Se_2,Se_8); Sf<-c(Sf_4,Sf_3,Sf_2,Sf_8)
+Sg<-c(Sg_4,Sg_3,Sg_2,Sg_8); Sh<-c(Sh_4,Sh_3,Sh_2,Sh_8); Si<-c(Si_4,Si_3,Si_2,Si_8)
+Sl<-c(Sl_4,Sl_3,Sl_2,Sl_8); Sm<-c(Sm_4,Sm_3,Sm_2,Sm_8); Sn<-c(Sn_4,Sn_3,Sn_2,Sn_8)
 
-Sa<-c(Sa_4,Sa_3,Sa_2,Sa_8)
-Sb<-c(Sb_4,Sb_3,Sb_2,Sb_8)
-Sc<-c(Sc_4,Sc_3,Sc_2,Sc_8)
-Sd<-c(Sd_4,Sd_3,Sd_2,Sd_8)
-Se<-c(Se_4,Se_3,Se_2,Se_8)
-Sf<-c(Sf_4,Sf_3,Sf_2,Sf_8)
+
 par(mfrow=c(2,3))
 plotRGB(Sa, stretch="lin", main="January 2022")
-plotRGB(Sb, stretch="lin", main="March 2022")
 plotRGB(Sc, stretch="lin", main="May 2022")
-plotRGB(Sd, stretch="lin", main="July 2022")
 plotRGB(Se, stretch="lin", main="September 2022")
-plotRGB(Sf, stretch="lin", main="October 2022")
+plotRGB(Sg, stretch="lin", main="January 2021")
+plotRGB(Si, stretch="lin", main="May 2021")
+plotRGB(Sm, stretch="lin", main="September 2021")
 
-
+# NDVI COMPARISON
 ndvia<-(Sa_8-Sa_4)/(Sa_8+Sa_4)
 ndvib<-(Sb_8-Sb_4)/(Sb_8+Sb_4)
 ndvic<-(Sc_8-Sc_4)/(Sc_8+Sc_4)
 ndvid<-(Sd_8-Sd_4)/(Sd_8+Sd_4)
 ndvie<-(Se_8-Se_4)/(Se_8+Se_4)
 ndvif<-(Sf_8-Sf_4)/(Sf_8+Sf_4)
-par(mfrow=c(2,3))
-plot(ndvia, col=rev(terrain.colors(10)), main = "NDVI Jan", range=c(0,1))
-plot(ndvib, col=rev(terrain.colors(10)), main = "NDVI Mar", range=c(0,1))
-plot(ndvic, col=rev(terrain.colors(10)), main = "NDVI May", range=c(0,1))
-plot(ndvid, col=rev(terrain.colors(10)), main = "NDVI July", range=c(0,1))
-plot(ndvie, col=rev(terrain.colors(10)), main = "NDVI Sept", range=c(0,1))
-plot(ndvif, col=rev(terrain.colors(10)), main = "NDVI Oct", range=c(0,1))
-
-## HISTOGRAMS
-par(mfrow=c(2,3))
-ha<-hist(ndvia, main = "Jan NDVI values", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 50, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 0.1), labels = seq(0, 1, 0.1))
-hb<-hist(ndvib, main = "Mar NDVI values", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 50, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 0.1), labels = seq(0, 1, 0.1))
-hc<-hist(ndvic, main = "May NDVI values", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 50, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 0.1), labels = seq(0, 1, 0.1))
-hd<-hist(ndvid, main = "July NDVI values", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 50, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 0.1), labels = seq(0, 1, 0.1))
-he<-hist(ndvie, main = "Sept NDVI values", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 50, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 0.1), labels = seq(0, 1, 0.1))
-hf<-hist(ndvif, main = "Oct NDVI values", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 50, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 0.1), labels = seq(0, 1, 0.1))
+ndvig<-(Sg_8-Sg_4)/(Sg_8+Sg_4)
+ndvih<-(Sh_8-Sh_4)/(Sh_8+Sh_4)
+ndvii<-(Si_8-Si_4)/(Si_8+Si_4)
+ndvil<-(Sl_8-Sl_4)/(Sl_8+Sl_4)
+ndvim<-(Sm_8-Sm_4)/(Sm_8+Sm_4)
+ndvin<-(Sn_8-Sn_4)/(Sn_8+Sn_4)
+veg1 <- clamp(ndvia, 0.4, values=FALSE); veg2 <- clamp(ndvib, 0.4, values=FALSE) 
+veg3 <- clamp(ndvic, 0.4, values=FALSE); veg4 <- clamp(ndvid, 0.4, values=FALSE) 
+veg5 <- clamp(ndvie, 0.4, values=FALSE); veg6 <- clamp(ndvif, 0.4, values=FALSE) 
+veg7 <- clamp(ndvig, 0.4, values=FALSE); veg8 <- clamp(ndvih, 0.4, values=FALSE) 
+veg9 <- clamp(ndvii, 0.4, values=FALSE); veg10 <- clamp(ndvil, 0.4, values=FALSE) 
+veg11 <- clamp(ndvim, 0.4, values=FALSE); veg12 <- clamp(ndvin, 0.4, values=FALSE) 
+d1 <- veg1-veg7; d2 <- veg2-veg8; d3 <- veg3-veg9
+d4 <- veg4-veg10; d5 <- veg5-veg11; d6 <-veg6-veg12
 
 par(mfrow=c(2,3))
-h_1<-hist(ndvia, main = "Jan", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 2, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 1), labels = seq(0, 1, 1))
-h_2<-hist(ndvib, main = "March", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 2, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 1), labels = seq(0, 1, 1))
-h_3<-hist(ndvic, main = "May", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 2, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 1), labels = seq(0, 1, 1))
-h_4<-hist(ndvid, main = "July", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 2, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 1), labels = seq(0, 1, 1))
-h_5<-hist(ndvie, main = "Sept", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 2, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 1), labels = seq(0, 1, 1))
-h_6<-hist(ndvif, main = "Oct", xlab = "NDVI", ylab= "Frequency", 
-     col = "wheat", xlim = c(0, 1),  breaks = 2, xaxt = "n") 
-axis(side=1, at = seq(0, 1, 1), labels = seq(0, 1, 1))
+cl<-colorRampPalette(c("blue","white","red"))(100)
+plot(d1, col=cl, main = "NDVI Jan", range=c(-0.4,0.4))
+plot(d2, col=cl, main = "NDVI Mar", range=c(-0.4,0.4))
+plot(d3, col=cl, main = "NDVI May", range=c(-0.4,0.4))
+plot(d4, col=cl, main = "NDVI July", range=c(-0.4,0.4))
+plot(d5, col=cl, main = "NDVI Sept", range=c(-0.4,0.4))
+plot(d6, col=cl, main = "NDVI Nov", range=c(-0.4,0.4))
+
+
 
 
 
